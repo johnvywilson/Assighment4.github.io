a0e6f55b049ebd7e3c51caf7f4579ac86b896446
johnvywilson <139486606+johnvywilson@users.noreply.github.com>
2023-12-19 21:49:52 -0500

    Update raw-text.js

--- raw-text.js
+++ raw-text.js
@@ -1,47 +1,44 @@
-1. COMPLETE VARIABLE AND FUNCTION DEFINITIONS
+var customName = document.getElementById('customname');
+var randomize = document.querySelector('.randomize');
+var story = document.querySelector('.story');
 
-const customName = document.getElementById('customname');
-const randomize = document.querySelector('.randomize');
-const story = document.querySelector('.story');
-
-function randomValueFromArray(array){
-  const random = Math.floor(Math.random()*array.length);
-  return array[random];
+function getRandomFromArray(array){
+  const randomIndex = Math.floor(Math.random() * array.length);
+  return array[randomIndex];
 }
 
-2. RAW TEXT STRINGS
+var storyText = 'It was 94 degrees Fahrenheit outside, so :insertx: went for a walk. When they arrived at :inserty:, they stared in horror for a few moments, then :insertz:. Bob witnessed the entire event but remained unsurprised — :insertx: weighs 300 pounds, and it was a hot day.';
 
-It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.
+var insertXOptions = ['Willy the Goblin', 'Big Daddy', 'Father Christmas'];
+var insertYOptions = ['the soup kitchen', 'Disneyland', 'the White House'];
+var insertZOptions = ['spontaneously combusted', 'melted into a puddle on the sidewalk', 'turned into a slug and crawled away'];
 
-Willy the Goblin
-Big Daddy
-Father Christmas
+randomize.addEventListener('click', generateResult);
 
-the soup kitchen
-Disneyland
-the White House
+function generateResult() {
+    var newStory = storyText;
 
-spontaneously combusted
-melted into a puddle on the sidewalk
-turned into a slug and crawled away
+    var xItem = getRandomFromArray(insertXOptions);
+    var yItem = getRandomFromArray(insertYOptions);
+    var zItem = getRandomFromArray(insertZOptions);
 
-3. EVENT LISTENER AND PARTIAL FUNCTION DEFINITION
+    newStory = newStory.replace(':insertx:', xItem);
+    newStory = newStory.replace(':insertx:', xItem);
+    newStory = newStory.replace(':inserty:', yItem);
+    newStory = newStory.replace(':insertz:', zItem);
 
-randomize.addEventListener('click', result);
+    if (customName.value !== '') {
+        var name = customName.value;
+        newStory = newStory.replace('Bob', name);
+    }
 
-function result() {
+    if (document.getElementById("uk").checked) {
+        var weight = Math.round(21) + ' stone';
+        var temperature = Math.round(34) + ' degrees Celsius';
+        newStory = newStory.replace('300 pounds', weight);
+        newStory = newStory.replace('94 degrees Fahrenheit', temperature);
+    }
 
-  if(customName.value !== '') {
-    const name = customName.value;
-
-  }
-
-  if(document.getElementById("uk").checked) {
-    const weight = Math.round(300);
-    const temperature =  Math.round(94);
-
-  }
-
-  story.textContent = ;
-  story.style.visibility = 'visible';
+    story.textContent = newStory;
+    story.style.visibility = 'visible';
 }
